2896674390c08a3bcfe14d09c52d236a27dd70a2
Bullen <julius.jansson@elev.ga.ntig.se>
2023-09-12 11:10:09 +0200

    liugliugluih

---
+++ Interactive story/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Vem ska du gifta dig med, Ropenis eller Adaladis");
+String svar2;
+String svar1 = Console.ReadLine();
+
+if (svar1.ToLower() == "ropenis"){
+Console.WriteLine("Du och Ropenis kommer nu leva lyckliga i resten av era liv.");
+}
+if (svar1.ToLower() == "adaladis"){
+Console.WriteLine("Du har nu blivit skickad till helvetet (Spånga)\n Nu har du två val: Gå hem till Adalidis eller hoppa ner i en monsterpöl.");
+
+String svar2 = Console.ReadLine();
+
+if (svar2.ToLower() == "monsterpöl"){
+Console.WriteLine("Du fick en snabb och smärtfri död");
+}
+if (svar2.ToLower() == "Adaladis"){
+    
+}
+
+}
+
+Console.ReadLine();
+
